50f58c3824245e955bcdd085bf5d024e364a6fcd
Jacobinq <210064650@aston.ac.uk>
2023-02-15 11:46:48 +0000

    Fixed

--- src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -6,13 +6,10 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.Customer;
+import com.example.demo.User.*;;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-  List<Customer> findByLastName(String lastName);
-
-  Customer findById(long id);
-    
 }
 
 
